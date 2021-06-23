eb07ca5032f11077d81dc839978387a886624fa7
s1500729 <dicenull.dev@gmail.com>
2021-06-23 22:16:53 +0900

    カーソルのストア表示を切り出した

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,9 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_flame_cookie_clicker/components/cursor_store_tile.dart';
 import 'package:flutter_flame_cookie_clicker/components/store_select_widget.dart';
 import 'package:flutter_flame_cookie_clicker/controllers/cookie_controller.dart';
-import 'package:flutter_flame_cookie_clicker/controllers/cursor_controller.dart';
 import 'package:flutter_flame_cookie_clicker/cookie_clicker.dart';
-import 'package:flutter_flame_cookie_clicker/gen/assets.gen.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -26,25 +25,7 @@ class App extends HookWidget {
                 ),
               ),
               StoreSelectWidget(),
-              ListTile(
-                leading: Assets.images.cursor.image(),
-                title: Text(
-                  'Cursor',
-                  style: TextStyle(fontSize: 24),
-                ),
-                subtitle: Text(
-                  useProvider(cursorProvider).nextCost.toString(),
-                ),
-                trailing: Text(
-                  useProvider(cursorProvider).count.toString(),
-                  style: TextStyle(fontSize: 24),
-                ),
-                onTap: useProvider(canBuyCursorProvider).state
-                    ? () {
-                        context.read(cursorProvider.notifier).buyCursor();
-                      }
-                    : null,
-              ),
+              CursorStoreTile(),
             ],
           ),
         ),
